bacedd9651a5df0fc3ae63956e7eb2af34707cef
MaiThiThanh1821050938MNMTKT <mtthanhsky@gmail.com>
2022-03-28 07:32:23 +0700

    First_commit_MTT938_2022

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MaiThiThanh2022938.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
